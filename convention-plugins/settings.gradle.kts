a3bdf6a6ae47d454336c4a38862a22a030286b83
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-05 17:38:09 +0500

    gcp-base: configure settings.gradle.kts convention-plugins/base module

---
+++ convention-plugins/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = URI("https://androidx.dev/storage/compose-compiler/repository/")
+        }
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../../gradle/libs.versions.toml"))
+        }
+    }
+}
+rootProject.name = "base"
